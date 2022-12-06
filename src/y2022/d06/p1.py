98a12288c411c47c74939b7d8c05c76bb4b92b7a
tainn <tainn@protonmail.com>
2022-12-06 20:38:19 +0100

    day 6 dual sources

---
+++ src/y2022/d06/p1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def detect_first_marker(chunk: int) -> int:
+    with open("input", "r") as rf:
+        data: str = rf.read().strip()
+
+    for idx, _ in enumerate(data):
+        if len(set(data[idx:idx + chunk])) == chunk:
+            return idx + chunk
+
+
+if __name__ == "__main__":
+    print(detect_first_marker(4))
---
+++ src/y2022/d06/p2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from p1 import detect_first_marker
+
+if __name__ == "__main__":
+    print(detect_first_marker(14))
